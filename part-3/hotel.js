16f6dc137b7a80caae79f284dacb68d36d3fc6d9
Roel Cabrera <roel.cabrera@gmail.com>
2017-08-14 12:14:00 -0700

    booking button will update information provided in modal

--- part-3/hotel.js
+++ part-3/hotel.js
@@ -1,6 +1,9 @@
 const book = document.getElementsByClassName('book');
 const modal = document.getElementById('booking');
 const close = document.getElementsByClassName('close')[0];
+const modalRoomNum = document.getElementById('roomNum');
+const modalPrice = document.getElementById('pricePerNight');
+const modalTotal = document.getElementById('total');
 
 let room = [];
 
@@ -10,7 +13,6 @@ for (var i = 0; i < book.length; i++) {
         room.num = b.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
         room.capacity = b.parentNode.previousElementSibling.previousElementSibling.innerHTML;
         room.price = b.parentNode.previousElementSibling.innerHTML;
-
         bookingContents();
     });
 }
@@ -22,9 +24,15 @@ for (var i = 0; i < book.length; i++) {
 //will "close" the Modal
 
 function bookingContents() {
+    modalRoomNum.innerHTML = 'Room ' + room.num
+    modalPrice.innerHTML = room.price + '/ Night'
     modal.style.display = "block";
-};
+}
 
+function nights(quan) {
+	let total = quan * parseFloat(room.price.slice(1));
+	modalTotal.innerHTML = '$ ' + total;
+}
 
 close.addEventListener('click', () => {
     modal.style.display = "none";
